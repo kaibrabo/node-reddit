85e4ec97a126d281cb891dde29304830ec3453c4
Kai Brabo <kaibrabo@gmail.com>
2018-09-22 00:10:55 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const routeConfig = require('./config/route-config.js');
+const appConfig = require('./config/main-config.js');
+
+appConfig.init();
 routeConfig.init(app);
 
 // Exports App Module
