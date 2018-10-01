7f60990aff96f4ad1c751b5d156531e3fc5bebcd
Kai Brabo <kaibrabo@gmail.com>
2018-09-30 21:11:43 -0700

    before topic spec

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,12 +5,13 @@ const base = 'http://localhost:3000';
 describe('routes : static', () => {
     // Root route
     describe('GET /', () => {
-        it('should return status code 200', (done) => {
+        it('should return status code 200', () => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 done();
             });
-        });
+        }); 
+        // https://stackoverflow.com/questions/22604644/jasmine-async-callback-was-not-invoked-within-timeout-specified-by-jasmine-defa
     });
 
     // Another route
