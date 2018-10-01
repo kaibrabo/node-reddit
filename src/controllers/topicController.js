4e50d0308a53bf195ff488abd4e62ce3dbc36577
Kai Brabo <kaibrabo@gmail.com>
2018-09-30 21:30:27 -0700

    /topics index, tests pass

---
+++ src/controllers/topicController.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     index(req, res, next) {
+//         res.render('static/index', {title: 'Welcome To Node'});
+//     }
+// };
+
+module.exports = {
+    index(req, res, next) {
+        res.send('TODO: list all topics');
+    }
+};
+
